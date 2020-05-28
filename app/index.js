e8eb2da837f1482a6e0958b3f1f263a2b926884a
Diego Caetano <diego.caetano@outlook.com>
2020-05-28 14:21:10 -0300

    kick off

---
+++ app/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World! This is a huge step, this his V3!!!'));
+
+const server = app.listen(3000, () => {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
