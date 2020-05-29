c0580090f63bb42f187a154ea83c47f79226e986
Diego Caetano <diego.caetano@outlook.com>
2020-05-29 15:41:40 -0300

    updated image

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 //abcd
-app.get('/', (req, res) => res.send('Hello World! This is a huge step, this his V3!!!'));
+app.get('/', (req, res) => res.send('Hello World! I quit if it works'));
 
 const server = app.listen(3000, () => {
     const host = server.address().address;
