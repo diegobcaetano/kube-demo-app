85e48a5df6b43c3e00ed365e4b2fcaaaffd95b6f
Diego Caetano <diego.caetano@outlook.com>
2020-06-12 11:40:27 -0300

    updated app

--- app/index.js
+++ app/index.js
@@ -1,21 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-//abcd
-app.get('/', (req, res) => {
-    res.json({
-        message: "This image please",
-        someEnvVariables: {
-            database: {
-                host: process.env.DATABASE_HOSTNAME,
-                user: process.env.DATABASE_USERNAME
-            }
-        }
-    })
+const axios = require('axios');
+
+app.get("/", async (req, res) => {
+  const seller = await axios.get("kube-demo-seller");
+  console.log(seller.data);
+  res.json({
+    message: "Let's link the containers!!",
+    someEnvVariables: {
+      database: {
+        host: process.env.DATABASE_HOSTNAME,
+        user: process.env.DATABASE_USERNAME,
+      },
+    },
+  });
 });
 
 const server = app.listen(3000, () => {
-    const host = server.address().address;
-    const port = server.address().port;
+  const host = server.address().address;
+  const port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+  console.log("Example app listening at http://%s:%s", host, port);
 });
