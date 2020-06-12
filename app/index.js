51d3d44583d446925951b9bccebfd4b935581a1e
Diego Caetano <diego.caetano@outlook.com>
2020-06-12 14:07:07 -0300

    omg

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 
 app.get("/", async (req, res) => {
   try {
-    request("kube-demo-seller.demo-service", function (error, response, body) {
+    request("http://kube-demo-seller.demo-service", function (error, response, body) {
       console.error("error:", error); // Print the error if one occurred
       console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
       console.log("body:", body); // Print the HTML for the Google homepage.
