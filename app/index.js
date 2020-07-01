d622ab60acaa90ae95c3ff59063f3cb619f222c0
Diego Caetano <diego.caetano@outlook.com>
2020-07-01 14:18:37 -0300

    ccc

--- app/index.js
+++ app/index.js
@@ -42,7 +42,7 @@ app.get("/", async (req, res) => {
   }
   console.log("some info coming from the demo app")
   res.status(202).json({
-    message: "Istio GO - bbb",
+    message: "Istio GO - ccc",
     someEnvVariables: {
       database: {
         host: process.env.DATABASE_HOSTNAME,
