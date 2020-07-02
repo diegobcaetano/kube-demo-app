319bf07980a29e2d05b33294fd32d471e10ddd3f
Diego Caetano <diego.caetano@outlook.com>
2020-07-02 14:00:30 -0300

    updated

--- app/index.js
+++ app/index.js
@@ -42,7 +42,7 @@ app.get("/", async (req, res) => {
   }
   console.log("some info coming from the demo app")
   res.status(203).json({
-    message: "Istio GO - eee",
+    message: "Istio GO - fff",
     someEnvVariables: {
       database: {
         host: process.env.DATABASE_HOSTNAME,
